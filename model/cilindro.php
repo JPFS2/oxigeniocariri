a5838159fb70f86c0f5b2f83453abd5d8701a460
Marlon Ferreira <marlonfsantos@outlook.com.br>
2023-02-19 23:29:47 -0300

    Inserindo cilindo e gravando arquivo na pasta

--- model/cilindro.php
+++ model/cilindro.php
@@ -1,29 +1,29 @@
 <?php
-session_start();
+
 include('conexao.php');
 
-if(empty($_POST['numerocilindro']) || empty($_POST['endereco'])) {
-	header('Location: ../login.php');
-	exit();
+$numero = filter_input(INPUT_POST, 'numerocilindro');
+$fab = filter_input(INPUT_POST, 'fabricacao');
+$venc = filter_input(INPUT_POST, 'vencimento');
+$obs = filter_input(INPUT_POST, 'obs');
+
+if (isset($_FILES['arquivo']['tmp_name'])){
+    $endereco = '../files/' . $numero . '.' . substr($_FILES['arquivo']['name'], -3);
+    move_uploaded_file($_FILES['arquivo']['tmp_name'],$endereco);
 }
 
-$numerocilindro = mysqli_real_escape_string($conexao, $_POST['numerocilindro']);
-$fabricacao = mysqli_real_escape_string($conexao, $_POST['fabricacao']);
-$vencimento = mysqli_real_escape_string($conexao, $_POST['vencimento']);
-$obs = mysqli_real_escape_string($conexao, $_POST['obs']);
-$endereco = mysqli_real_escape_string($conexao, $_POST['endereco']);
+$_FILES['arquivo']['tmp_name'] = null;
 
-$query = "select usuario from usuario where usuario = '{$usuario}' and senha = md5('{$senha}')";
+$query = "INSERT INTO cilindro(codigo, fabricacao, validade, observacao, endereco) VALUES ('{$numero}','{$fab}','{$venc}','{$obs}','{$endereco}')";
 
-$result = mysqli_query($conexao, $query);
-$row = mysqli_num_rows($result);
+if(mysqli_query($conexao, $query)){
+    $_SESSION['aviso'] = "Cadastrado com sucesso";
+    header("Location: ../painel.php");
 
-if($row == 1) {
-	$_SESSION['mensagem'] = "Cadastrado com sucesso";
-	header('Location: ../painel.php');
-	exit();
-} else {
-	$_SESSION['nao_autenticado'] = true;
-    header('Location: ../painel.php');
-	exit();
+}else{
+    $_SESSION['aviso'] = "Erro".$query;
 }
+
+
+
+echo $_FILES['arquivo']['name'];
