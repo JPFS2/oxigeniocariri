feced5f6dc85229772f78acefbbb37cf408da001
Marlon Ferreira <marlonfsantos@outlook.com.br>
2023-03-08 23:48:45 -0300

    Atualização do Panel 1

--- login.php
+++ login.php
@@ -61,7 +61,7 @@ include('model/conexao.php');
 
 <!-- ======= Footer ======= -->
 <section class="gradient-form" style="background-color: #eee;">
-    <section class="vh-100">
+    <section class="vh-100 ">
         <div class="container-fluid h-custom">
             <div class="row d-flex justify-content-center align-items-center h-100">
                 <div class="col-md-9 col-lg-6 col-xl-5">
